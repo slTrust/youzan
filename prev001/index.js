7c4d143fa8995279c83c33260b19b47a179bd025
huangjx <trustfor@sina.cn>
2018-01-16 23:23:49 +0800

    js调试和  页面调试

--- prev001/index.js
+++ prev001/index.js
@@ -1,32 +1,17 @@
 let url = 'http://rapapi.org/mockjsdata/23334/index/hotList'
+//请求这个模块   
+let axios = require('axios')
 
-// // let axios = require('axios')
-
-// axios.post(url,{
-//   pageNum: 1,
-//   pageSize: 10
-// }).then(res=>{ debugger
-//   console.log(res.data.lists)
-// })
-
-// let Mock = require('mockjs')
-// console.log(Mock.mock({
-//   "number|1-100.2": 1
-// }))
-// 
-
-new Vue({
-    el: '#app',
-    data: {
-        lists: null
-    },
-    created() {
-        axios.post(url, {
-            pageNum: 1,
-            pageSize: 10
-        }).then(res => {
-            
-            this.lists = res.data.lists
-        })
-    }
+axios.get(url,{
+  pageNum: 1,
+  pageSize: 10
+}).then(res=>{
+  console.log(res.data.lists)
 })
+
+let Mock = require('mockjs')
+console.log(Mock.mock({
+  "number|1-100.2": 1
+}))
+
+
