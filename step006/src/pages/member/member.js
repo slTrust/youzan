13b4f02ffff1913766e3ba8295c6518e9d9478dc
huangjx <trustfor@sina.cn>
2018-05-23 22:19:38 +0800

    目录结构改善，路由引入，根组件注入

--- step006/src/pages/member/member.js
+++ step006/src/pages/member/member.js
@@ -1,2 +1,26 @@
-import './member_base.css'
-import './member.css'
+
+import Vue from 'vue';
+import Router from 'vue-router';
+
+// 使用路由
+Vue.use(Router);
+
+let routes = [
+    {
+        path:'/',
+        component:require('./components/member.vue')
+    }
+];
+
+// 创建路由实例
+let router = new Router({
+    routes
+})
+
+// 根组件的注入
+
+new Vue({
+    el:'#app',
+    router
+})
+
