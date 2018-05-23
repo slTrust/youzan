9833f03814c370ad20eecd5e5afbadf1bb489c7b
huangjx <trustfor@sina.cn>
2018-05-23 23:36:03 +0800

    地址的数据渲染

--- step006/src/pages/member/components/all.vue
+++ step006/src/pages/member/components/all.vue
@@ -1,15 +1,24 @@
 <template>
     <div class="container " style="min-height: 597px;">
-        <div class="block-list address-list section section-first js-no-webview-block">
-            <a class="block-item js-address-item address-item " @click="toEdit">
-                <div class="address-title">tony 13112345678</div>
-                <p>广东省珠海市香洲区南方软件园</p>
+        <!-- 地址存在则循环渲染 -->
+        <div class="block-list address-list section section-first js-no-webview-block"
+            v-if="lists&&lists.length">
+            <!-- 默认地址isDefault -->
+            <a class="block-item js-address-item address-item " 
+                v-for="list in lists"   
+                :key="list.id"             
+                @click="toEdit"
+                :class="{'address-item-default':list.isDefault}"
+            >
+                <div class="address-title">{{list.name}} {{list.tel}}</div>
+                <p>{{list.provinceName}}{{list.cityName}}{{list.districtName}}{{list.address}}</p>
                 <a class="address-edit" href="javascript:;"></a>
             </a>
-            <a class="block-item js-address-item address-item address-item-default" href="https://pfmarket.youzan.com/user/address/form?m_alias=3nu78u467kddj&amp;id=69150193&amp;from=">
-                <div class="address-title">tony 13112345678</div>
-                <p>北京市北京市东城区天安门</p>
-            </a>
+          
+        </div>
+        <!-- 没有地址给予提示 -->
+        <div  v-if="lists&&!lists.length">
+            没有地址，请添加
         </div>
         <div class="block stick-bottom-row center">
             <router-link class="btn btn-blue js-no-webview-block js-add-address-btn" to="/address/form">
