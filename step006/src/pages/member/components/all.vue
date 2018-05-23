d25a88c231b6c2caeba94bcac61fd89ed0d5e5e3
huangjx <trustfor@sina.cn>
2018-05-23 23:08:53 +0800

    新增地址的路由添加 ，地址的编辑按钮添加

--- step006/src/pages/member/components/all.vue
+++ step006/src/pages/member/components/all.vue
@@ -4,6 +4,7 @@
             <a class="block-item js-address-item address-item " href="https://pfmarket.youzan.com/user/address/form?m_alias=3nu78u467kddj&amp;id=69150287&amp;from=">
                 <div class="address-title">tony 13112345678</div>
                 <p>广东省珠海市香洲区南方软件园</p>
+                <a class="address-edit" href="javascript:;"></a>
             </a>
             <a class="block-item js-address-item address-item address-item-default" href="https://pfmarket.youzan.com/user/address/form?m_alias=3nu78u467kddj&amp;id=69150193&amp;from=">
                 <div class="address-title">tony 13112345678</div>
@@ -11,9 +12,9 @@
             </a>
         </div>
         <div class="block stick-bottom-row center">
-            <a class="btn btn-blue js-no-webview-block js-add-address-btn" href="https://pfmarket.youzan.com/user/address/form?m_alias=3nu78u467kddj&amp;from=">
+            <router-link class="btn btn-blue js-no-webview-block js-add-address-btn" to="/address/form">
                 新增地址
-            </a>
+            </router-link>
         </div>
     </div>
 </template>
